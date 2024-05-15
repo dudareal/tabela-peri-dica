672cddb225ff9056fc71e5f22833248b1c001f87
Eduarda Real <dudarealferreira07@gmail.com>
2024-05-15 17:55:06 -0300

    Tabela Periódica

---
+++ tabela periodica.py
@@ -0,0 +1,10 @@
+sigla = input("Digite a sigla do elemento químico:")
+sigla = sigla.upper()
+if sigla == "H":
+    print("1, Hidrogênio, 1.008")
+elif sigla == "He":
+    print("2, Hélio, 4.0026")
+elif sigla == "Li":
+    print("3, Lítio, 6.94")
+else:
+    print("Sem informações no momento.")
